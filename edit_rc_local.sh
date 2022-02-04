50f2318d52e9c7e0979475e45d02e6515f1fc020
Marlon Couto Ribeiro <84450525+marloncori@users.noreply.github.com>
2022-02-04 10:32:52 +0100

    Update edit_rc_local.sh

--- edit_rc_local.sh
+++ edit_rc_local.sh
@@ -4,4 +4,4 @@
 ## edit this file because after booting up again #####
 ## RPi GPIO go back to their default state as input ##
 ######################################################
-sudo echo "gpio -g mode 5 out\ngpio -g mode 6 out\ngpio -g mode 13 out\ngpio -g mode 19 out\n" >> ~/etc/rc.local
+sudo echo "gpio -g mode 5 out\ngpio -g mode 6 out\ngpio -g mode 13 out\ngpio -g mode 19 out\ngpio -g mode 10 out\ngpio mode 1 pwm\nexit 0" >> ~/etc/rc.local
